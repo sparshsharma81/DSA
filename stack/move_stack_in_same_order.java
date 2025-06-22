ab78a3afc5d611bf9d521bcfa7a19212d9186439
sparshsharma81 <81arvindarora@gmail.com>
2025-06-22 22:56:14 +0530

    change2

--- stack/move_stack_in_same_order.java
+++ stack/move_stack_in_same_order.java
@@ -1,7 +1,16 @@
 import java.util.*;
 public class move_stack_in_same_order {
     public static void main(String args[]){
-        Stack<Integer>s1 = new Stack<>(1,2,3,4);
+        Scanner sc = new Scanner(System.in);
+
+        Stack<Integer>s1 = new Stack<>();
+        int n;
+        System.out.println("Enter the number");
+        n = sc.nextInt();
+        for(int i=1;i<n;i++){
+            int a = sc.nextInt();
+            s1.push(a);
+        }
         System.out.println(s1);
         
     }
