d0c8189fadf648d7f99d075b2e1714ab637f8f52
sparshsharma81 <81arvindarora@gmail.com>
2025-06-22 23:15:40 +0530

    reverse the stack

--- stack/move_stack_in_same_order.java
+++ stack/move_stack_in_same_order.java
@@ -7,11 +7,18 @@ public class move_stack_in_same_order {
         int n;
         System.out.println("Enter the number");
         n = sc.nextInt();
-        for(int i=1;i<n;i++){
+        for(int i=1;i<=n;i++){
             int a = sc.nextInt();
             s1.push(a);
         }
         System.out.println(s1);
-        
+        //now this is our stack..
+        //and we are putting the element to other stack
+        Stack<Integer>st2 = new Stack<>();
+        while(s1.size() > 0){
+            int x = s1.pop();
+            st2.push(x);
+        }
+        System.out.print("Now the second stack is : " + st2);
     }
 }
