a877a240897760aba13d70521b9fd70d46176aed
sparshsharma81 <81arvindarora@gmail.com>
2025-06-25 23:35:30 +0530

    stacks

--- stack/questions/balance_bracket.java
+++ stack/questions/balance_bracket.java
@@ -1,7 +1,18 @@
 public class balance_bracket {
+    public static boolean isBalance(String s){
+        Stack<Character>st = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == ')' && st.isEmpty())return false; 
+            if(s.charAt(i) =='(') st.push('(');
+
+        }
+        return st.isEmpty();
+    }
     public static void main(String args[]){
         //we are solving the balance bracket problems..
         //rule -- if open bracket... -- push it /
+        String s = "((())))";
+        System.out.print(isBalance(s));
         
     }
 }
