8ec45c8f12c0b8cff7ed18cc0a07299c1d5608f7
sparshsharma81 <81arvindarora@gmail.com>
2025-06-26 23:03:26 +0530

    stacks

--- stack/questions/largest_histogram.java
+++ stack/questions/largest_histogram.java
@@ -1,5 +1,7 @@
 public class largest_histogram{
     public static void main(String args[]){
-        System.out.println("Check the working");
+        //now we have to find out the largest histogram area..
+        //so for that we are working on previous smaller element and next smaller element
+        
     }
 }
