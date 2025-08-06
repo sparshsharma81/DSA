46bfd3f7463406583d15e243748ce6aca6d17f7a
sparshsharma81 <81arvindarora@gmail.com>
2025-08-06 10:29:31 +0530

    B Even Array

--- codeforce/B. Even Array/Main.java
+++ codeforce/B. Even Array/Main.java
@@ -3,9 +3,10 @@ public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        int a = 0;
-        int b =0;
+        
         while(t-- >0){
+            int a = 0;
+        int b =0;
             int n = sc.nextInt();
             int[] arr = new int[n];
             for(int i=0;i<n;i++){
@@ -13,16 +14,11 @@ public class Main{
             }
             
             for(int i=0;i<n;i++){
-                if(arr[i] % 2 == 0){
-                    if(i % 2 != 0){
-                        a++;
-                    }
-                }
-                else{
-                    if(i % 2 == 0){
-                        b++;
-                    }
-                }
+              if(i % 2 ==0){
+                if(arr[i] % 2 !=0)a++;
+              }else{
+                if(arr[i] % 2 ==0)b++;
+              }
             }
             if(a == b){
                 System.out.println(a);
