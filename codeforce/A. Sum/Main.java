57188c5d82b3fbed48e41fc2a4d6bb82a2b9d53f
sparshsharma81 <81arvindarora@gmail.com>
2025-08-13 23:21:13 +0530

    sum codeforce

---
+++ codeforce/A. Sum/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- >0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            if(a + b == c || b + c == a || c + a == b){
+                System.out.print("YES");
+            }
+            else System.out.print("NO");
+        }
+    }
+}
