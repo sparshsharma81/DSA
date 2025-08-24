5a192e83b04aa3999311e3d7d8368cbffec87fd3
sparshsharma81 <81arvindarora@gmail.com>
2025-08-24 11:32:51 +0530

    Codeforce A. Vlad and the Best of Five

---
+++ codeforce/A. Vlad and the Best of Five/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        while(a-->0){
+            String s = sc.next();
+            int a1 = 0;
+            for(char c : s.toCharArray()){
+                if(c == 'A')a1++;
+                
+            }
+            if(c == 3)System.out.println("A");
+            else System.out.println("B");
+        }
+    }
+}
