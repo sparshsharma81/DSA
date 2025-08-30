c0b78b9ed66e4ba7f0f84c7101d4fdb91fa8fdab
sparshsharma81 <81arvindarora@gmail.com>
2025-08-30 22:33:51 +0530

    codeforce

---
+++ codeforce/A. Game with Integers/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-->0){
+            int move = 10;
+            int a = sc.nextInt();
+            
+
+    }
+
+    }
+}
