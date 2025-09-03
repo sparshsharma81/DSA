a4a84027f0423eb7f5489853d0926957d76f21b6
sparshsharma81 <81arvindarora@gmail.com>
2025-09-03 18:55:53 +0530

    codeforce

---
+++ codeforce/A. Presents/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        int[] ans = new int[n];
+        for(int i=0;i<n;i++){
+            ans[arr[i]] = i+1;
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(ans[i]);
+            if(i !=n-1)System.out.print(" ");
+        }
+
+    }
+}
