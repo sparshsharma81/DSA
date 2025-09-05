33ff56eceea3ecbbaeb9d07f58f355765140a910
sparshsharma81 <81arvindarora@gmail.com>
2025-09-05 22:18:37 +0530

    Codeforce

--- codeforce/A. A+B/Main.java
+++ codeforce/A. A+B/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
         String s = sc.next();
         int a = Character.getNumericValue(s.charAt(0));
         int b = Character.getNumericValue(s.charAt(2));
         System.out.println(a + b);
+        }
     }
 }
