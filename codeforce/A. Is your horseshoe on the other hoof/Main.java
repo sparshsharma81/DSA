7dc987f2e5ebfa7efce0485ee02bb9321f9534b6
sparshsharma81 <81arvindarora@gmail.com>
2025-09-13 19:46:26 +0530

    Codeforce

--- codeforce/A. Is your horseshoe on the other hoof/Main.java
+++ codeforce/A. Is your horseshoe on the other hoof/Main.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+import java.util.*;
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        HashSet<Integer> set = new HashSet<>();
+        set.add(a);
+        set.add(b);
+        set.add(c);
+        set.add(d);
+        System.out.println(set.size());
+    }
 }
