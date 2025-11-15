b01c3fd82787e58ab3da9625eada67e681914626
sparsh <130464884+sparshsharma81@users.noreply.github.com>
2025-11-15 16:30:36 +0530

    Dynamic programming tabulation -- memoization

---
+++ Dynamic Programming/climbing_stairs.java
@@ -0,0 +1,8 @@
+public class climbing_stairs {
+    public static void main(String[] args){
+        //now we tend to solve the problem climbing stairs using dynamic programming 
+        //so the basic approach to solve any dynamic prograaming
+        int n = 5; //number of stairs
+        int[] dp = new int[n+1];
+    }
+}
