134a119c51ebab664ec3a187b3cd3b34cc32940f
sparshsharma81 <81arvindarora@gmail.com>
2025-12-27 18:43:20 +0530

    dsa

---
+++ Dynamic Programming/Graph/dfs.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class BFSGraph {
+    private int V; // number of vertices
+    private ArrayList<ArrayList<Integer>> adj;
+
+    BFSGraph(int V) {
+        this.V = V;
+        adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+    }
+
+    void addEdge(int u, int v) {
+        adj.get(u).add(v);
+        adj.get(v).add(u); // for undirected graph
+    }
+
+    void bfs(int start) {
+        boolean[] visited = new boolean[V];
+        Queue<Integer> q = new LinkedList<>();
+
+        visited[start] = true;
+        q.add(start);
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            System.out.print(node + " ");
+
+            for (int neighbor : adj.get(node)) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    q.add(neighbor);
+                }
+            }
+        }
+    }
+}
