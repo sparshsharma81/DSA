555aeabd070ca19e43108fc9d5bfffc683fa06c9
sparshsharma81 <81arvindarora@gmail.com>
2026-01-23 23:20:31 +0530

    nse

---
+++ stack/NEXT AND SMALLER/nextsmaller.java
@@ -0,0 +1,9 @@
+package NEXT AND SMALLER;
+
+
+public class nextsmaller {
+    public static void main(String[] args){
+        Stack<Integer>st = new Stack<>();
+        int[] arr = {4,5,2,10,8};
+    }
+}
