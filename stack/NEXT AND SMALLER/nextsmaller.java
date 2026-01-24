e6dbc35f009d8f32f84522cb8a1adb2fcf79bbdc
sparshsharma81 <81arvindarora@gmail.com>
2026-01-25 03:15:45 +0530

    dsa:nge

--- stack/NEXT AND SMALLER/nextsmaller.java
+++ stack/NEXT AND SMALLER/nextsmaller.java
@@ -2,6 +2,23 @@ package NEXT AND SMALLER;
 
 
 public class nextsmaller {
+    private static int[] nextSmaller(int[] arr){
+        int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && st.peek() >= arr[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                ans[i] = -1;
+            }else{
+                ans[i] = st.peek();
+            }
+            st.push(arr[i]);
+        }
+        return ans;
+    }
     public static void main(String[] args){
         Stack<Integer>st = new Stack<>();
         int[] arr = {4,5,2,10,8};
