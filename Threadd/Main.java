597ca75ea78d037983c05e913f022dfa1f4246df
sparshsharma81 <81arvindarora@gmail.com>
2026-02-08 23:32:17 +0530

    Runnable

---
+++ Threadd/Main.java
@@ -0,0 +1,12 @@
+class MyTask implements Runnable {
+    public void run() {
+        System.out.println("Thread running");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Thread t = new Thread(new MyTask());
+        t.start();
+    }
+}
