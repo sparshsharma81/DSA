79f457ac27e388a977dbd187bcc9278d5b0c3f45
sparshsharma81 <81arvindarora@gmail.com>
2026-05-23 22:47:02 +0530

    Rotate array

---
+++ check_array_rotated.java
@@ -0,0 +1,12 @@
+public class check_array_rotated {
+   class Solution {
+    public boolean check(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i] > nums[(i+1)%n] && ++count>1) return false;
+        }
+        return count<=1;
+    }
+}
+}
