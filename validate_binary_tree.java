48e9832e7fe79cc8886f17d169c7ac4483133b6d
sparshsharma81 <81arvindarora@gmail.com>
2026-06-11 23:21:37 +0530

    binary tree

--- validate_binary_tree.java
+++ validate_binary_tree.java
@@ -1,21 +1,6 @@
 import javax.swing.tree.TreeNode;
 
 public class validate_binary_tree {
-    /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     class Pair{
         boolean isbst = true; //initiallly bst true hai kyuki vo null hai
