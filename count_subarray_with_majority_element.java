b050e2a295cabb879745489db32a0dc2d73aa1ac
sparshsharma81 <81arvindarora@gmail.com>
2026-06-25 22:27:00 +0530

    count subarray with majority element

---
+++ count_subarray_with_majority_element.java
@@ -0,0 +1,18 @@
+public class count_subarray_with_majority_element{
+
+    public int countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+        int a =0;
+        for(int i=0;i<n;i++){
+            int tar =0;
+            for(int j=i;j<n;j++){
+                if(nums[j] == target)tar++;
+                
+                int len = j - i + 1;
+                if(tar > len/2)a++;
+            }
+        }
+        return a;
+    
+}
+}
