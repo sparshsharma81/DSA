3d18de9724d182a638900295cdd3e2cf2d9e08f6
sparshsharma81 <81arvindarora@gmail.com>
2026-07-13 22:30:08 +0530

    Repeated DNA Sequence

---
+++ Repeated_DNA_sequence.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Repeated_DNA_sequence {
+    public List<String> findRepeatedDnaSequences(String s) {
+        int i=0;
+        int j=10;
+        List<String>ans = new ArrayList<>();
+        // String finl = s.substring(0,11);
+        HashMap<String,Integer>h1 = new HashMap<>();
+        while(j<=s.length()){
+            StringBuilder s2 = new StringBuilder(s.substring(i,j));
+            ArrayList<String>ar = new ArrayList<>();
+            ar.add(s2.toString());
+            Collections.sort(ar);
+            // h1.put(s2.toString());
+            h1.put(ar.get(0),h1.getOrDefault(ar.get(0),0)+1);
+            i++;
+            j++;
+        }
+        // System.out.println(h1);
+        for(Map.Entry<String,Integer>h : h1.entrySet()){
+            if(h.getValue()>1)ans.add(h.getKey());
+        }
+        return ans;
+
+}
+}
