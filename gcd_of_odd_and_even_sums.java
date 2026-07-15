ed1d7901ba8621a679eb3c96db5a294cb4e61cd6
sparshsharma81 <81arvindarora@gmail.com>
2026-07-15 22:39:20 +0530

    gcd of odd and even sums

---
+++ gcd_of_odd_and_even_sums.java
@@ -0,0 +1,17 @@
+public class gcd_of_odd_and_even_sums{
+    class Solution {
+    private int gcd(int a, int b){
+        while(b!=0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    public int gcdOfOddEvenSums(int n) {
+        int a = n * n;
+        int b = n * (n+1);
+        return gcd(a,b);
+    }
+}
+}
